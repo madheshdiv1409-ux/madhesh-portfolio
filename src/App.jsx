93cc1d08bc4e0c3d7b88f2dfc92bd85b7976bd4a
Madhesh waran <madheshdiv.1409@gmail.com>
2026-08-01 19:40:15 +0530

    Fix import issues

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Navbar from "./components/navbar/Navbar";
 import Hero from "./components/Hero/Hero";
 import About from "./components/About/About";
 import Skills from "./components/Skills/Skills";
-import Projects from "./components/projects/projects";s
+import Projects from "./components/projects/projects";
 import Contact from "./components/contact/Contact";
 import Footer from "./components/footer/Footer";
 
