78fe4c8613ae79c106ee248f78ec53bab14dabba
Madhesh waran <madheshdiv.1409@gmail.com>
2026-08-01 18:45:34 +0530

    Fix Footer import case

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import About from "./components/About/About";
 import Skills from "./components/Skills/Skills";
 import Projects from "./components/Projects/Projects";
 import Contact from "./components/Contact/Contact";
-import Footer from "./components/Footer/Footer";
+import Footer from "./components/footer/Footer";
 
 function App() {
 
