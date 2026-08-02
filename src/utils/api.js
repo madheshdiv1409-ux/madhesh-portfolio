33e45736c6c8877cb9f8cdb3b6dd661b724c9475
Madhesh waran <madheshdiv.1409@gmail.com>
2026-08-02 23:53:37 +0530

    connect frontend with deployed backend

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,10 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "http://127.0.0.1:8000/api",
-    headers: {
-        "Content-Type": "application/json",
-    },
+    baseURL: "https://madhesh-portfolio-backend.onrender.com/api"
 });
 
 export default API;
